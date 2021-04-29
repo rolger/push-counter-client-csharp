210eff4247d79034634e6ff2c517d879bd664d8f
LaszloGarab <laszlo.garab@nagarro.com>
2021-04-29 17:15:48 +0200

    Refactor test to get european parrot class directly

--- Parrot.Tests/ParrotTest.cs
+++ Parrot.Tests/ParrotTest.cs
@@ -48,7 +48,7 @@ namespace Parrot.Tests
         [Fact]
         public void GetSpeedOfEuropeanParrot()
         {
-            var parrot = new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
+            var parrot = new EuropeanParrot();
             Assert.Equal(12.0, parrot.GetSpeed());
         }
     }
--- Parrot/EuropeanParrot.cs
+++ Parrot/EuropeanParrot.cs
@@ -2,7 +2,7 @@
 
 namespace Parrot
 {
-    internal class EuropeanParrot : Parrot
+    public class EuropeanParrot : Parrot
     {
         public EuropeanParrot() : base(ParrotTypeEnum.EUROPEAN,0,0,false)
         {
