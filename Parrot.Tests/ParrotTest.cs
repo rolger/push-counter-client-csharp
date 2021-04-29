146449561bcb9f7a3e9b1f0eb56afa6c7d47f71b
LaszloGarab <laszlo.garab@nagarro.com>
2021-04-29 17:34:48 +0200

    Refactoring Test Methods

--- Parrot.Tests/ParrotTest.cs
+++ Parrot.Tests/ParrotTest.cs
@@ -27,21 +27,21 @@ namespace Parrot.Tests
         [Fact]
         public void GetSpeedOfAfricanParrot_With_No_Coconuts()
         {
-            var parrot = new Parrot(ParrotTypeEnum.AFRICAN, 0, 0, false);
+            var parrot = Parrot.AfricanParrot(0);
             Assert.Equal(12.0, parrot.GetSpeed());
         }
 
         [Fact]
         public void GetSpeedOfAfricanParrot_With_One_Coconut()
         {
-            var parrot = new Parrot(ParrotTypeEnum.AFRICAN, 1, 0, false);
+            var parrot = Parrot.AfricanParrot(1);
             Assert.Equal(3.0, parrot.GetSpeed());
         }
 
         [Fact]
         public void GetSpeedOfAfricanParrot_With_Two_Coconuts()
         {
-            var parrot = new Parrot(ParrotTypeEnum.AFRICAN, 2, 0, false);
+            var parrot = Parrot.AfricanParrot(2);
             Assert.Equal(0.0, parrot.GetSpeed());
         }
 
